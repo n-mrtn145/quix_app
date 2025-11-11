e676e84cd21fb9388a4127056a1e9dd364ace9c0
noah <noah.martin@hm.edu>
2025-11-12 00:39:20 +0100

    delete test

--- src/test/java/com/quix/quix/QuixApplicationTests.java
+++ src/test/java/com/quix/quix/QuixApplicationTests.java
@@ -1,13 +1,1 @@
-package com.quix.quix;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class QuixApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
